4090e40d0898cc7f7d79dad67ca5101d6453dc77
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-03-09 09:53:04 -0300

    Update breakingRecords.js

---
+++ problem-solving/breakingRecords/breakingRecords.js
@@ -0,0 +1,18 @@
+function breakingRecords(scores) {
+    let maior = scores[0]
+    let menor = scores[0]
+    let contMa = 0
+    let contMe = 0
+    scores.forEach(element => {
+        if (element > maior) {
+            maior = element
+            contMa++
+        } else if (element < menor) {
+            menor = element
+            contMe++
+        }
+    });
+    return [contMa, contMe]
+}
+
+breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42])
